e753795890c5e851a0ee229b9b279b3cc803b288
Sergey <s.mikhalskiy@game-insight.com>
2023-06-04 20:31:02 +0300

    рабочая версия с закоментированным массивом

--- Program.cs
+++ Program.cs
@@ -2,8 +2,19 @@
 // длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
 // либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями,
 // лучше обойтись исключительно массивами.
+
 string[] arrString;
-arrString = new string[5];
+string[] secString;
+arrString = new string[5]; // {"123","rerfdf", "34", "szdfw43", "1"};
+for (int j = 0; j < arrString.Length; j++)
+    arrString[j] = Console.ReadLine ()!;
+secString = new string[arrString.Length];
+int count = 0;
 for (int i = 0; i < arrString.Length; i++)
-    arrString[i] = Console.ReadLine()!;
-Console.WriteLine("[" + string.Join(" ", arrString) + "]");
+    {
+    if (arrString[i].Length <= 3){
+        secString[count] = arrString[i];
+        count ++;
+        }
+    }
+Console.WriteLine("[" + string.Join(" ", arrString) + "]" + "[" + string.Join(" ", secString) + "]");
